88a566eeebe843c80861be63129085a13371bcda
SapunkovAA <AASapunkovv@gmail.com>
2018-07-09 13:48:30 +0300

    add_store_router

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import {createStore, applyMiddleware} from 'redux';
+import reducers from '../reducers/';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createBrowserHistory} from 'history';
+import {connectRouter} from 'connected-react-router';
+
+export const history = createBrowserHistory();
+
+const middleware = [thunk];
+
+const enchancer = composeWithDevTools(applyMiddleware(...middleware));
+
+
+export default function configureStore(persistedState = {}) {
+    return createStore(
+        connectRouter(history)(reducers),
+        persistedState, enchancer
+    );
+}
